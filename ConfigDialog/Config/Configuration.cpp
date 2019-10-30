0409d4c3b3e796f296d70944a1be9b5ec5f46caa
JayDalton <github@t-jay.net>
2019-10-30 02:46:48 +0100

    added AppConfig

--- ConfigDialog/Config/Configuration.cpp
+++ ConfigDialog/Config/Configuration.cpp
@@ -258,5 +258,73 @@ const std::string& Configuration::getStringParameter(const std::string& ident) c
    return {};
 }
 
+///////////////////////////////////////////////////////////////////////////
+
+using namespace std::string_literals;
+
+ApplicationConfig::ApplicationConfig()
+/*: Configuration{ std::string{""}, std::string{""} }*/
+{
+   //registerBaseParameter(BaseParameter{ 
+   //   DEFAULT_MATRIX_IMPORT, "Standard Matrix Datei", 
+   //   std::string{"current"}, 4096, 0
+   //   }
+   //);
+
+   //registerBaseParameter(BaseParameter{ 
+   //   DEFAULT_MATRIX_FOLDER, "Standard Matrix Ordner", 
+   //   fs::path{"application.cfg"}, 4096, 0
+   //   }
+   //);
+
+   //registerBaseParameter(BaseParameter{ 
+   //   DEFAULT_LOGFILE_FOLDER, "Standard Logfile", 
+   //   fs::path{"logfiles/dialog.log"}, 4096, 0
+   //   }
+   //);
+
+   //registerListParameter(ListParameter{
+   //   MATRIX_FOLDER_FILELIST, "Standard Datei Liste", 
+   //   {
+   //      "path/to/next/sub/folder/1"s,
+   //      "path/to/next/sub/folder/2"s,
+   //      "path/to/next/sub/folder/3"s,
+   //      "path/to/next/sub/folder/4"s,
+   //   }
+   //   });
+}
+
+std::string ApplicationConfig::getDefaultImportMatrix() const
+{
+   return getStringParameter(DEFAULT_MATRIX_IMPORT);
+}
+
+void ApplicationConfig::setDefaultImportMatrix(const std::string& file)
+{
+   setParameter(DEFAULT_MATRIX_IMPORT, file);
+}
+
+std::string ApplicationConfig::getDefaultMatrixFolder() const
+{
+   return getStringParameter(DEFAULT_MATRIX_FOLDER);
+}
+
+void ApplicationConfig::setDefaultMatrixFolder(std::string_view file)
+{
+}
+
+std::vector<std::string> ApplicationConfig::getFolderFilelist() const
+{
+   const ListParameter& param = getListParameter(MATRIX_FOLDER_FILELIST);
+   //if (std::holds_alternative<std::vector<std::string>>(param.m_current))
+   //{
+   //   return std::get<ListParameter>(param);
+   //}
+   return std::vector<std::string>();
+}
+
+void ApplicationConfig::setFolderFilelist(const std::vector<std::string>& list)
+{
+}
 
 // Codepage: UTF-8 (ÜüÖöÄäẞß)
--- ConfigDialog/Config/Configuration.h
+++ ConfigDialog/Config/Configuration.h
@@ -32,4 +32,25 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////
 
+struct ApplicationConfig : public Configuration
+{
+   ApplicationConfig();
+
+   std::string getDefaultImportMatrix() const;
+   void setDefaultImportMatrix(const std::string& file);
+
+   std::string getDefaultMatrixFolder() const;
+   void setDefaultMatrixFolder(std::string_view file);
+
+   std::vector<std::string> getFolderFilelist() const;
+   void setFolderFilelist(const std::vector<std::string>& list);
+
+private:
+   static constexpr char* DEFAULT_MATRIX_IMPORT{"matrixImport"};
+   static constexpr char* DEFAULT_MATRIX_FOLDER{"matrixFolder"};
+   static constexpr char* DEFAULT_LOGFILE_FOLDER{"loggingFolder"};
+   static constexpr char* MATRIX_FOLDER_FILELIST{"folderFileFist"};
+};
+
+
 // Codepage: UTF-8 (ÜüÖöÄäẞß)
